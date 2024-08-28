b15e181fb17d1885eeb37f565e5e99642dff0d8a
kobetaniyama <163136342+kobetaniyama@users.noreply.github.com>
2024-08-28 07:59:49 -0300

    jss

---
+++ inde.js
@@ -0,0 +1,33 @@
+var nome = 'Felipe'
+
+let idade = 17;
+
+const pi = 3.14;
+
+//
+let a = 5;
+let b = 10;
+let soma = a + b; //
+console.log(soma);
+
+//
+let a = 5;
+let b = 10;
+
+function multiplica(x,y) {
+    return x * y;
+}
+let produto = multiplica ( a, b) ; 
+console.log(produto) ; //50
+
+//
+let frutas = ['Apple', 'Banana', 'Cherry'];
+console.log(frutas[1]); // Banana
+
+//
+let pessoa = {
+    primeiroNome : 'Felipe',
+    sobrenome ; 'Yuji'
+    age:17
+};
+console.log (pessoa.primeiroNome); // John
